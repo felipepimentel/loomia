65366fad948fb40c4cf76582157e08c88b128bbd
Felipe Pimentel <fpimentel88@gmail.com>
2024-09-23 23:08:27 -0300

    feat: restructure project files and update paths; remove unused files and enhance preload API

--- src/app-desktop/main.ts
+++ src/app-desktop/main.ts
@@ -1,34 +1,40 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow } from 'electron';
 import path from 'path';
 
-async function createWindow() {
-  const win = new BrowserWindow({
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true,  // Segurança: Isola contextos do renderer e main
-      nodeIntegration: false,  // Desabilitar integração Node.js no renderer
+      preload: path.join(__dirname, '../preload/preload.js'),
     },
   });
 
-  if (process.env.NODE_ENV === 'development') {
-    win.loadURL('http://localhost:3000');
-    win.webContents.openDevTools();
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    win.loadFile(path.join(__dirname, '../dist/index.html'));
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
   }
 
-  // Escutar mensagens do renderer
-  ipcMain.on('channel-name', (event, data) => {
-    console.log(`Received ${data} from renderer process`);
-    // Enviar uma resposta de volta ao renderer
-    event.reply('channel-reply', 'Reply from main process');
-  });
-}
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+};
 
-app.whenReady().then(createWindow);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
 
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -36,7 +42,12 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
