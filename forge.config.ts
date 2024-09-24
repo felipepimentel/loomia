c440c18493d49c47eec9fd7f09d620a86772c7b0
Felipe Pimentel <fpimentel88@gmail.com>
2024-09-24 15:45:48 -0300

    feat: migrate Vite configuration files to TypeScript; remove old JavaScript config files

--- forge.config.ts
+++ forge.config.ts
@@ -18,19 +18,19 @@ const config: ForgeConfig = {
       build: [
         {
           entry: 'src/app-desktop/main.ts',
-          config: 'vite.main.config.ts',
+          config: 'vite.main.config.mts',
           target: 'main',
         },
         {
           entry: 'src/preload/preload.ts',
-          config: 'vite.preload.config.ts',
+          config: 'vite.preload.config.mts',
           target: 'preload',
         },
       ],
       renderer: [
         {
           name: 'main_window',
-          config: 'vite.renderer.config.ts',
+          config: 'vite.renderer.config.mts',
         },
       ],
     }),
